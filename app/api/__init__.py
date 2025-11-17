10f5b11ca42393ad8249c458f4d67d0e13c7219f
Brennan <brennan.pan3@gmail.com>
2025-11-17 17:50:54 -0500

    "Added scraping"

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -13,6 +13,7 @@ from .routes.simulations import simulation_bp
 from .routes.simulations import simulation_bp
 from .routes.challenges import challenges_bp
 from .routes.courses import courses_bp
+from .routes.scraping_events import scraping_events_bp
 # from .routes.advising import advising_bp
 
 def register_blueprints(app):
@@ -22,4 +23,5 @@ def register_blueprints(app):
     app.register_blueprint(me_bp, url_prefix="/api/v1")
     app.register_blueprint(simulation_bp, url_prefix="/api/v1")
     app.register_blueprint(courses_bp, url_prefix="/api/v1")
+    app.register_blueprint(scraping_events_bp, url_prefix="/api/v1") 
     # register others as you implement them
